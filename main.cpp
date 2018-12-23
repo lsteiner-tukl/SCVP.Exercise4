c2b8ea4f411935245e0bd35b0ee460e112cb6eeb
Lukas Steiner <lsteiner@rhrk.uni-kl.de>
2018-12-23 21:48:27 +0100

    Single memory bank

--- main.cpp
+++ main.cpp
@@ -8,23 +8,21 @@
 SC_MODULE(toplevel)
 {
 public:
-    transition<1,2> t1;
-    transition<2,1> t2;
-    transition<1,1> t3;
-    place<1,1> p1, p2, p3, p4;
+    transition<1,1> ACT, RD, PRE, WR;
+    place<1,1> IDLE, ACTIVE;
 
-    SC_CTOR(toplevel) : t1("t1"), t2("t2"), t3("t3"),
-                        p1(1), p2(0), p3(0), p4(0)
+    SC_CTOR(toplevel) : ACT("ACT"), RD("RD"), PRE("PRE"), WR("WR"),
+                        IDLE(1), ACTIVE(0)
     {
         SC_THREAD(process);
-        t1.in.bind(p1);
-        t1.out.bind(p2); // 0
-        t1.out.bind(p3); // 1
-        t2.in.bind(p2);  // 0
-        t2.in.bind(p4);  // 1
-        t2.out.bind(p1);
-        t3.in.bind(p3);
-        t3.out.bind(p4);
+        ACT.in(IDLE);
+        ACT.out(ACTIVE);
+        RD.in(ACTIVE);
+        RD.out(ACTIVE);
+        PRE.in(ACTIVE);
+        PRE.out(IDLE);
+        WR.in(ACTIVE);
+        WR.out(ACTIVE);
     }
 
     void process()
@@ -32,13 +30,17 @@ public:
         while(true)
         {
             wait(10,SC_NS);
-            t1.fire();
+            ACT.fire();
             wait(10,SC_NS);
-            t2.fire();
+            ACT.fire();
             wait(10,SC_NS);
-            t3.fire();
+            RD.fire();
             wait(10,SC_NS);
-            t2.fire();
+            WR.fire();
+            wait(10,SC_NS);
+            PRE.fire();
+            wait(10,SC_NS);
+            ACT.fire();
             sc_stop();
         }
     }
